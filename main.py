dea661e05e2e389646203b31be704cf7ba73e3f9
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2023-11-29 08:23:13 +0100

    added training and testing loop

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import os
-import pandas as pd
-import numpy as np
 import torch
 from torch import nn
-from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 
 
 device = (
@@ -33,12 +29,19 @@ test_data = datasets.CIFAR10(
     target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
 )
 
+learning_rate = 0.001
+batch_size = 64
+epochs = 5
+
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(32*32, 512),
+            nn.Linear(32*32*3, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
@@ -51,9 +54,48 @@ class NeuralNetwork(nn.Module):
         return logits
 
 model = NeuralNetwork().to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-X = torch.rand(1, 32, 32, device=device)
-logits = model(X)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:5f} \t {current:5d}/{size:5d}")
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    model.eval()
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    
+    test_loss /= num_batches
+    correct /= size
+    print(f"Accuracy: {(100*correct):0.01f}% \t Average loss: {test_loss:5f}")
+
+for e in range(epochs):
+    print(f"Epoch {e+1}\n")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Finished!")
+
+# X = torch.rand(1, 32, 32, device=device)
+# logits = model(X)
+# pred_probab = nn.Softmax(dim=1)(logits)
+# y_pred = pred_probab.argmax(1)
+# print(f"Predicted class: {y_pred}")
